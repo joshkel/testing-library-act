b1c83ab192772b96d32e7138280a60b5225a9b61
Josh Kelley <joshkel@gmail.com>
2024-04-09 15:01:49 -0400

    Add fireEvents version for comparison

--- demo.test.jsx
+++ demo.test.jsx
@@ -1,10 +1,18 @@
 /**@jest-environment jsdom */
 
 import TextField from '@mui/material/TextField';
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import { userEvent } from '@testing-library/user-event';
 
-it('uses a text field', async () => {
+it('uses a text field via fireEvent', async () => {
+    render(<TextField label="Value"/>);
+
+    const input = screen.getByLabelText('Value');
+    fireEvent.change(input, {target: {value: 'Hello'}});
+    expect(input.value).toEqual('Hello');
+});
+
+it('uses a text field via user-event', async () => {
     render(<TextField label="Value"/>);
 
     const input = screen.getByLabelText('Value');
